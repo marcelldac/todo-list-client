21e7af033c43f593b3da0b8b04bb3bacbaf9ab60
marcelldac <dacmarcell@gmail.com>
2023-11-14 08:53:07 -0300

    test: modify title should

--- client/cypress/e2e/home.cy.js
+++ client/cypress/e2e/home.cy.js
@@ -3,6 +3,6 @@
 describe("home", () => {
   it("webapp should be online", () => {
     cy.visit("http://localhost:5173");
-    cy.title().should("eq", "Vite + React");
+    cy.title().should("eq", "TODO with Cypress");
   });
 });
