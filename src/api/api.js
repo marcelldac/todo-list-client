5dffb4271ee644de386559d0708035d447b4ef1f
marcelldac <dacmarcell@gmail.com>
2023-11-25 19:43:45 -0300

    chore: modify api link

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: "https://todolist-api-hdhx.onrender.com/",
 });
